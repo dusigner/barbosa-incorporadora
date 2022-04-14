2160e99165debb28996cae57899abb6b57c1302a
Eduardo Pacheco Soares <e.soares@samsung.com>
2022-04-13 21:02:44 -0300

    Version 0.1

--- gulpfile.js
+++ gulpfile.js
@@ -3,65 +3,81 @@ const fileinclude = require('gulp-file-include');
 const server = require('browser-sync').create();
 const { watch, series } = require('gulp');
 const sass = require('gulp-sass');
+const webp = require('gulp-webp');
 sass.compiler = require('node-sass');
+const htmlmin = require('gulp-htmlmin');
+const autoprefixer = require('gulp-autoprefixer');
 
 const paths = {
   scripts: {
     src: './src/',
-    dest: './dist/'
+    dest: './build/'
   }
 };
 
-// Reload Server
 async function reload() {
   server.reload();
 }
 
-// Sass compiler
 async function compileSass() {
-  gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./assets/css'));
+  gulp.src('./src/sass/**/*.scss')
+  .pipe(sass(
+    {outputStyle: 'compressed'}
+    ).on('error', sass.logError))
+    .pipe(gulp.dest('./src/assets/css'));
 }
 
-// Copy assets after build
+async function webpImage() {
+  gulp.src(['./src/assets/**/*.jpg','./src/assets/**/*.png'])
+    .pipe(webp())
+    .pipe(gulp.dest('./build/webp'));
+}
+exports.webp = webpImage;
+
 async function copyAssets() {
-  gulp.src(['assets/**/*'])
+  gulp.src(['src/assets/**/*'])
     .pipe(gulp.dest(paths.scripts.dest));
 }
 
-// Build files html and reload server
+
 async function buildAndReload() {
   await includeHTML();
   await copyAssets();
+  await webpImage();
   reload();
 }
 
 async function includeHTML(){
   return gulp.src([
-    '*.html',
-    '!header.html', // ignore
-    '!footer.html' // ignore
+    './src/*.html',
+    '!./src/_formulario.html',
+    '!./src/_header_secundary.html',
+    '!./src/_header.html',
+    '!./src/_menu-links.html',
+    '!./src/_menu-social.html',
+    '!./src/_scripts-jquery.html',
+    '!./src/_scripts-menu.html',
+    '!./src/_scripts-slick.html',
+    '!./src/_slider.html',
     ])
     .pipe(fileinclude({
       prefix: '@@',
       basepath: '@file'
     }))
+    .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest(paths.scripts.dest));
 }
 exports.includeHTML = includeHTML;
 
 exports.default = async function() {
-  // Init serve files from the build folder
   server.init({
     server: {
-      baseDir: paths.scripts.dest
+      baseDir: paths.scripts.dest,
+      directory: true,
+      index: "index.html"
     }
   });
-  // Build and reload at the first time
   buildAndReload();
-  // Watch Sass task
-  watch('./sass/**/*.scss',  series(compileSass));
-  // Watch task
-  watch(["*.html","assets/**/*"], series(buildAndReload));
+  watch('./src/sass/**/*.scss',  series(compileSass));
+  watch(["src/*.html","src/assets/**/*"], series(buildAndReload));
 };
